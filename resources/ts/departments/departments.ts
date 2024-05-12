09f69d87321e81eda2620c4b46014b8a2835ed6c
Ryan Ali <ryanali456@gmail.com>
2024-05-12 16:45:28 +0800

    implement add department

--- resources/ts/departments/departments.ts
+++ resources/ts/departments/departments.ts
@@ -1,6 +1,7 @@
 import { createApp, ref } from 'vue'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import { StatusCodes } from 'http-status-codes'
 
 createApp({
   compilerOptions: {
@@ -11,6 +12,10 @@ createApp({
     'column': Column,
   },
   setup() {
+    const form = ref({
+      name: '',
+    })
+
     const products = ref([
       {
         name: 'test',
@@ -19,8 +24,22 @@ createApp({
         name: 'test1',
       },
     ])
+
+    const onSubmitCreate = async () => {
+      console.log('test')
+      const response = await fetch('/admin/departments', {
+        method: 'POST',
+        body: JSON.stringify(form.value),
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+      })
+      if (response.status === StatusCodes.OK) {
+        alert('New Department Added')
+      }
+    }
     return {
       products,
+      form,
+      onSubmitCreate,
     }
   },
 }).mount('#departmentsPage')
