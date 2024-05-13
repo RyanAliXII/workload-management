ce40b06e55330cba53c3125bfe26e181ed62fd21
Ryan Ali <ryanali456@gmail.com>
2024-05-13 16:36:39 +0800

    display subjects

--- resources/ts/subjects/subjects.ts
+++ resources/ts/subjects/subjects.ts
@@ -1,8 +1,15 @@
 import { StatusCodes } from 'http-status-codes'
 import { createApp, onMounted, ref } from 'vue'
 import { toStructuredErrors } from '../utils/form.js'
-
+import { Subject } from '#types/subjects'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { toReadableDatetime } from '../utils/date.js'
 createApp({
+  components: {
+    'data-table': DataTable,
+    'column': Column,
+  },
   compilerOptions: {
     delimiters: ['${', '}'],
   },
@@ -13,6 +20,7 @@ createApp({
     }
     const form = ref({ ...INITIAL_FORM })
     const errors = ref({})
+    const subjects = ref<Subject[]>([])
     onMounted(() => {
       fetchSubjects()
       $('#addSubjectModal').on('hidden.bs.modal', () => {
@@ -30,6 +38,13 @@ createApp({
         headers: new Headers({ 'Content-Type': 'application/json' }),
       })
       const responseBody = await response.json()
+      subjects.value =
+        responseBody?.subjects?.map((s: any) => ({
+          id: s.id,
+          name: s.name,
+          createdAt: new Date(s.createdAt),
+          updatedAt: new Date(s.updatedAt),
+        })) ?? []
     }
     const onSubmitCreate = async () => {
       errors.value = {}
@@ -40,6 +55,7 @@ createApp({
       })
       const responseBody = await response.json()
       if (response.status === StatusCodes.OK) {
+        fetchSubjects()
         $('#addSubjectModal').modal('hide')
         toastr.success('Subject has been added.')
         resetForm()
@@ -56,6 +72,8 @@ createApp({
       onSubmitCreate,
       onSubmitUpdate,
       errors,
+      subjects,
+      toReadableDatetime,
     }
   },
 }).mount('#subjectPage')
