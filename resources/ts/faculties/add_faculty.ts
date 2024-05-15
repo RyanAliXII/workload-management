8684fe37e95886a18c8f48b3d085359585aa8f01
Ryan Ali <ryanali456@gmail.com>
2024-05-15 20:57:23 +0800

    create default form values

--- resources/ts/faculties/add_faculty.ts
+++ resources/ts/faculties/add_faculty.ts
@@ -2,7 +2,20 @@ import { createApp, ref } from 'vue'
 
 createApp({
   setup() {
-    const form = ref({})
+    const form = ref({
+      givenName: '',
+      middleName: '',
+      surname: '',
+      dateOfBirth: new Date(),
+      TIN: '',
+      positionId: 0,
+      employmentStatus: '',
+      fundSourceId: 0,
+      education: [],
+      email: '',
+      mobileNumber: '',
+      password: '',
+    })
     const errors = ref({})
 
     return {
