1f0d7f233918ca7027dba8b285ae647faf206da2
Ryan Ali <ryanali456@gmail.com>
2024-05-21 19:36:25 +0800

    create migraton for event table and facilitator

---
+++ database/migrations/1716291111320_create_events_facilitators_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'event_facilitator'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('event_id').unsigned().index().references('id').inTable('event')
+      table.integer('faculty_id').unsigned().index().references('id').inTable('faculty')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
